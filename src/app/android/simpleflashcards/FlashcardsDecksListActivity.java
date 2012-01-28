157d8654e49b3e9a4631b8814e4771c47b257cc0
Artur Dryomov <me@ming13.ru>
2012-01-28 22:41:44 +0300

    Movements to filling lists of data in activities.

--- src/app/android/simpleflashcards/FlashcardsDecksListActivity.java
+++ src/app/android/simpleflashcards/FlashcardsDecksListActivity.java
@@ -1,17 +1,32 @@
 package app.android.simpleflashcards;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 public class FlashcardsDecksListActivity extends ListActivity
 {
-	private Context activityContext = this;
+	private final Context activityContext = this;
+	
+	private ArrayList<HashMap<String, Object>> decksData;
+	
+	private static final String LIST_ITEM_TEXT_ID = "text";
+	
+	public FlashcardsDecksListActivity() {
+		super();
+		
+		decksData = new ArrayList<HashMap<String, Object>>();
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,7 @@ public class FlashcardsDecksListActivity extends ListActivity
 		setContentView(R.layout.flashcards_decks);
 		
 		initializeActionbar();
+		initializeDecksList();
 	}
 	
 	private void initializeActionbar() {
@@ -42,4 +58,26 @@ public class FlashcardsDecksListActivity extends ListActivity
 			ActivityStarter.start(activityContext, FlashcardsDeckCreationActivity.class);
 		}
 	};
+	
+	private void initializeDecksList() {
+		SimpleAdapter decksAdapter = new SimpleAdapter(activityContext, decksData,
+			R.layout.flashcards_decks_list_item, new String[] { LIST_ITEM_TEXT_ID },
+			new int[] { R.id.text });
+		
+		setListAdapter(decksAdapter);
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	}
+	
+	private void addItemToDecksList(String text) {
+		HashMap<String, Object> deckItem = new HashMap<String, Object>();
+		
+		deckItem.put(LIST_ITEM_TEXT_ID, text);
+		
+		decksData.add(deckItem);
+	}
+	
+	private void updateDecksList() {
+		((SimpleAdapter) getListAdapter()).notifyDataSetChanged();
+	}
 }
--- src/app/android/simpleflashcards/FlashcardsListActivity.java
+++ src/app/android/simpleflashcards/FlashcardsListActivity.java
@@ -1,17 +1,33 @@
 package app.android.simpleflashcards;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 public class FlashcardsListActivity extends ListActivity
 {
-	private Context activityContext = this;
+	private final Context activityContext = this;
+	
+	private ArrayList<HashMap<String, Object>> flashcardsData;
+	
+	private static final String LIST_ITEM_FRONT_TEXT = "front_text";
+	private static final String LIST_ITEM_BACK_TEXT = "back_text";
+	
+	public FlashcardsListActivity() {
+		super();
+		
+		flashcardsData = new ArrayList<HashMap<String,Object>>();
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,7 @@ public class FlashcardsListActivity extends ListActivity
 		setContentView(R.layout.flashcards);
 		
 		initializeActionbar();
+		initializeFlashcardsList();
 	}
 	
 	private void initializeActionbar() {
@@ -42,4 +59,27 @@ public class FlashcardsListActivity extends ListActivity
 			ActivityStarter.start(activityContext, FlashcardCreationActivity.class);
 		}
 	};
+	
+	private void initializeFlashcardsList() {
+		SimpleAdapter flashcardsAdapter = new SimpleAdapter(activityContext, flashcardsData,
+			R.layout.flashcards_list_item, new String[] { LIST_ITEM_FRONT_TEXT, LIST_ITEM_BACK_TEXT },
+			new int[] { R.id.title, R.id.description });
+		
+		setListAdapter(flashcardsAdapter);
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	}
+	
+	private void addItemToFlashcardsList(String frontText, String backText) {
+		HashMap<String, Object> flashcardItem = new HashMap<String, Object>();
+		
+		flashcardItem.put(LIST_ITEM_FRONT_TEXT, frontText);
+		flashcardItem.put(LIST_ITEM_BACK_TEXT, backText);
+		
+		flashcardsData.add(flashcardItem);
+	}
+	
+	private void updateFlashcardsList() {
+		((SimpleAdapter) getListAdapter()).notifyDataSetChanged();
+	}
 }
