0fcae2f1d6be566d3a83ee277585af724bf375c0
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-02-02 22:21:47 +0300

    Worksheet insertion implemented.

--- src/app/android/simpleflashcards/spreadsheets/SpreadsheetsClient.java
+++ src/app/android/simpleflashcards/spreadsheets/SpreadsheetsClient.java
@@ -11,12 +11,15 @@ import app.android.simpleflashcards.spreadsheets.models.WorksheetFeed;
 
 import com.google.api.client.googleapis.GoogleHeaders;
 import com.google.api.client.googleapis.GoogleUrl;
+import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.xml.atom.AtomContent;
 import com.google.api.client.http.xml.atom.AtomParser;
 import com.google.api.client.xml.XmlNamespaceDictionary;
 
@@ -26,6 +29,18 @@ import com.google.api.client.xml.XmlNamespaceDictionary;
 
 public class SpreadsheetsClient
 {
+	private static final String APPLICATION_NAME = "Simple-Flashcards/0.0.1";
+	private static final XmlNamespaceDictionary DICTIONARY = new XmlNamespaceDictionary()
+		.set("", "http://www.w3.org/2005/Atom").set("app", "http://www.w3.org/2007/app")
+		.set("batch", "http://schemas.google.com/gdata/batch")
+		.set("docs", "http://schemas.google.com/docs/2007")
+		.set("gAcl", "http://schemas.google.com/acl/2007")
+		.set("gd", "http://schemas.google.com/g/2005")
+		.set("openSearch", "http://a9.com/-/spec/opensearch/1.1/")
+		.set("xml", "http://www.w3.org/XML/1998/namespace")
+		.set("gs", "http://schemas.google.com/spreadsheets/2006")
+		.set("gsx", "http://schemas.google.com/spreadsheets/2006/extended");
+
 	private String authToken;
 	HttpRequestFactory requestFactory;
 
@@ -41,16 +56,6 @@ public class SpreadsheetsClient
 
 	private class RequestInitializer implements HttpRequestInitializer
 	{
-		private static final String APPLICATION_NAME = "Simple-Flashcards/0.0.1";
-		private final XmlNamespaceDictionary DICTIONARY = new XmlNamespaceDictionary()
-			.set("", "http://www.w3.org/2005/Atom").set("app", "http://www.w3.org/2007/app")
-			.set("batch", "http://schemas.google.com/gdata/batch")
-			.set("docs", "http://schemas.google.com/docs/2007")
-			.set("gAcl", "http://schemas.google.com/acl/2007")
-			.set("gd", "http://schemas.google.com/g/2005")
-			.set("openSearch", "http://a9.com/-/spec/opensearch/1.1/")
-			.set("xml", "http://www.w3.org/XML/1998/namespace");
-
 		@Override
 		public void initialize(HttpRequest request) throws IOException {
 			request.setHeaders(buildHeaders());
@@ -70,22 +75,22 @@ public class SpreadsheetsClient
 
 	public SpreadsheetFeed getSpreadsheetFeed() {
 		HttpRequest request = buildGetRequest(SpreadsheetUrl.spreadsheetFeedUrl());
-		return processRequest(request, SpreadsheetFeed.class);
+		return processGetRequest(request, SpreadsheetFeed.class);
 	}
 
 	public SpreadsheetEntry getSpreadsheetEntry(String id) {
 		HttpRequest request = buildGetRequest(new SpreadsheetUrl(id));
-		return processRequest(request, SpreadsheetEntry.class);
+		return processGetRequest(request, SpreadsheetEntry.class);
 	}
 
 	public WorksheetFeed getWorksheetFeed(SpreadsheetEntry spreadsheet) {
 		HttpRequest request = buildGetRequest(spreadsheet.getWorksheetFeedUrl());
-		return processRequest(request, WorksheetFeed.class);
+		return processGetRequest(request, WorksheetFeed.class);
 	}
 
 	public CellFeed getCellFeed(WorksheetEntry worksheet) {
 		HttpRequest request = buildGetRequest(worksheet.getCellFeedUrl());
-		return processRequest(request, CellFeed.class);
+		return processGetRequest(request, CellFeed.class);
 	}
 
 	private HttpRequest buildGetRequest(GoogleUrl url) {
@@ -97,7 +102,7 @@ public class SpreadsheetsClient
 		}
 	}
 
-	private <T> T processRequest(HttpRequest request, Class<T> dataClass) {
+	private <T> T processGetRequest(HttpRequest request, Class<T> dataClass) {
 		try {
 			return processRequest(request).parseAs(dataClass);
 		}
@@ -106,14 +111,43 @@ public class SpreadsheetsClient
 		}
 	}
 
-	private HttpResponse processRequest(HttpRequest request) throws IOException {
-		HttpResponse response = request.execute();
+	public void insertWorksheet(SpreadsheetEntry spreadsheet, String title, int rowCount,
+		int columnCount) {
 
-		if (!response.isSuccessStatusCode()) {
-			throw exceptionFromUnsuccessfulStatusCode(response.getStatusCode());
+		WorksheetEntry entry = new WorksheetEntry();
+		entry.title = title;
+		entry.rowCount = rowCount;
+		entry.columnCount = columnCount;
+
+		AtomContent content = AtomContent.forEntry(DICTIONARY, entry);
+
+		HttpRequest request = buildPostRequest(spreadsheet.getWorksheetFeedUrl(), content);
+		processPostRequest(request);
+	}
+
+	private HttpRequest buildPostRequest(SpreadsheetUrl url, HttpContent content) {
+		try {
+			return requestFactory.buildPostRequest(url, content);
 		}
+		catch (IOException e) {
+			throw new SpreadsheetException(e);
+		}
+	}
 
-		return response;
+	private void processPostRequest(HttpRequest request) {
+		processRequest(request);
+	}
+
+	private HttpResponse processRequest(HttpRequest request) {
+		try {
+			return request.execute();
+		}
+		catch (HttpResponseException e) {
+			throw exceptionFromUnsuccessfulStatusCode(e.getResponse().getStatusCode());
+		}
+		catch (IOException e) {
+			throw new FailedRequestException(e);
+		}
 	}
 
 	private FailedRequestException exceptionFromUnsuccessfulStatusCode(int statusCode) {
