2947c7886a15db9a960f4b15936c9a4b2054bb53
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-02-09 18:41:08 +0300

    Get only documents owned by the authorized user.

--- src/app/android/simpleflashcards/googledocs/GoogleDocsUrl.java
+++ src/app/android/simpleflashcards/googledocs/GoogleDocsUrl.java
@@ -8,12 +8,14 @@ import com.google.api.client.googleapis.GoogleUrl;
 
 public class GoogleDocsUrl extends GoogleUrl
 {
-	private static final String TYPE_PART_DRAWING = "/-/drawing";
-	private static final String TYPE_PART_PRESENTATION = "/-/presentation";
-	private static final String TYPE_PART_SPREADSHEET = "/-/spreadsheet";
-	private static final String TYPE_PART_DOCUMENT = "/-/document";
 	private static final String ROOT_PATH = "https://docs.google.com/feeds";
 	private static final String DOCUMENTS_FEED_PART = "/default/private/full";
+	private static final String DIVIDER_PART = "/-";
+	private static final String TYPE_PART_DRAWING = "/drawing";
+	private static final String TYPE_PART_PRESENTATION = "/presentation";
+	private static final String TYPE_PART_SPREADSHEET = "/spreadsheet";
+	private static final String TYPE_PART_DOCUMENT = "/document";
+	private static final String OWNER_PART = "/mine";
 
 	public GoogleDocsUrl(String url) {
 		super(url);
@@ -24,6 +26,8 @@ public class GoogleDocsUrl extends GoogleUrl
 
 		builder.append(ROOT_PATH);
 		builder.append(DOCUMENTS_FEED_PART);
+		builder.append(DIVIDER_PART);
+		builder.append(OWNER_PART);
 
 		return new GoogleDocsUrl(builder.toString());
 	}
@@ -33,7 +37,9 @@ public class GoogleDocsUrl extends GoogleUrl
 
 		builder.append(ROOT_PATH);
 		builder.append(DOCUMENTS_FEED_PART);
+		builder.append(DIVIDER_PART);
 		builder.append(buildDocumentTypePart(type));
+		builder.append(OWNER_PART);
 
 		return new GoogleDocsUrl(builder.toString());
 	}
