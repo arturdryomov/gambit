c200ef9b342ebbd19f1009be5e7a42e76f1cfdbd
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-04-14 17:44:41 +0300

    RemoteCard and RemoteDeck data objects implemented.

---
+++ src/app/android/simpleflashcards/remotedecks/RemoteCard.java
@@ -0,0 +1,66 @@
+package app.android.simpleflashcards.remotedecks;
+
+
+public class RemoteCard
+{
+	private String frontSideText;
+	private String backSideText;
+
+	public String getFrontSideText() {
+		return frontSideText;
+	}
+
+	public void setFrontSideText(String text) {
+		frontSideText = text;
+	}
+
+	public String getBackSideText() {
+		return backSideText;
+	}
+
+	public void setBackSideText(String text) {
+		backSideText = text;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("{ %s : %s}", frontSideText, backSideText);
+	}
+
+	@Override
+	public int hashCode() {
+		// hashCode() is not intended to be used
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean equals(Object otherObject) {
+		if (this == otherObject) {
+			return true;
+		}
+
+		if (!(otherObject instanceof RemoteCard)) {
+			return false;
+		}
+
+		RemoteCard otherCard = (RemoteCard) otherObject;
+
+		if ((frontSideText == null) && (otherCard.frontSideText != null)) {
+			return false;
+		}
+
+		if ((frontSideText != null) && !frontSideText.equals(otherCard.frontSideText)) {
+			return false;
+		}
+
+		if ((backSideText == null) && (otherCard.backSideText != null)) {
+			return false;
+		}
+
+		if ((backSideText != null) && !backSideText.equals(otherCard.backSideText)) {
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ src/app/android/simpleflashcards/remotedecks/RemoteDeck.java
@@ -0,0 +1,66 @@
+package app.android.simpleflashcards.remotedecks;
+
+
+import java.util.List;
+
+
+public class RemoteDeck
+{
+	private String title;
+	private List<RemoteCard> cards;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<RemoteCard> getCards() {
+		return cards;
+	}
+
+	public void setCards(List<RemoteCard> cardsList) {
+		cards = cardsList;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("{ %s\n %s }", title, cards.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		// hashCode() is not intended to be used
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean equals(Object otherObject) {
+		if (this == otherObject) {
+			return true;
+		}
+
+		if (!(otherObject instanceof RemoteDeck)) {
+			return false;
+		}
+
+		RemoteDeck otherDeck = (RemoteDeck) otherObject;
+
+		if ((title == null) && (otherDeck.title != null)) {
+			return false;
+		}
+
+		if ((title != null) && !title.equals(otherDeck.title)) {
+			return false;
+		}
+
+		if (!cards.equals(otherDeck.cards)) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
