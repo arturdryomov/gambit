c9e01e63bc01c9ad482b5e144848cd4a5f6179ea
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-04-20 22:02:16 +0300

    Change database name to ‘Gambit'.

--- src/app/android/gambit/local/GambitDbOpenHelper.java
+++ src/app/android/gambit/local/GambitDbOpenHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class GambitDbOpenHelper extends SQLiteOpenHelper
 {
 	private static final int DATABASE_VERSION = 1;
-	private static final String DATABASE_NAME = "SimpleFlashcards";
+	private static final String DATABASE_NAME = "Gambit";
 
 	public GambitDbOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
