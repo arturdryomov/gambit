7641fcb0e6b9d63b9d04ee93fc515f4d9073848e
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-04-23 14:36:53 +0300

    Remove incorrect comment from DbFieldParams.CARD_ORDER_INDEX.
    CARD_ORDER_INDEX field cannot be unique, because cards are ordered separately
    for separate decks. Thus, there may be more than one card with equal order index.

--- src/app/android/gambit/local/DbFieldParams.java
+++ src/app/android/gambit/local/DbFieldParams.java
@@ -11,9 +11,6 @@ public class DbFieldParams
 	public static final String CARD_FRONT_SIDE_TEXT = "text not null";
 	public static final String CARD_BACK_SIDE_TEXT = "text not null";
 
-	// OrderIndex field should actually be unique, but it is hard to shuffle and
-	// reset indexes if the db strictly takes care of this. So, uniqueness should be
-	// guaranteed by the algorithm.
 	public static final String CARD_ORDER_INDEX = "int not null";
 
 	public static final String DB_LAST_UPDATE_TIME = "text not null unique";
