410195ec1fb97913ec39edeeaf489126f50ea7ba
Artur Dryomov <me@ming13.ru>
2012-04-28 18:09:16 +0300

    Remove unnecessary quotes in field parameter.

--- src/app/android/gambit/local/DbFieldParams.java
+++ src/app/android/gambit/local/DbFieldParams.java
@@ -7,7 +7,7 @@ class DbFieldParams
 	public static final String DECK_TITLE = "text not null unique";
 	public static final String DECK_NEXT_CARD_INDEX = "int not null";
 
-	public static final String CARD_DECK_ID = "integer not null references \"Decks\"(\"_id\")";
+	public static final String CARD_DECK_ID = "integer not null references Decks(_id)";
 	public static final String CARD_FRONT_SIDE_TEXT = "text not null";
 	public static final String CARD_BACK_SIDE_TEXT = "text not null";
 
