170d839ced49857084339eee035edc3b83ea558c
Artur Dryomov <me@ming13.ru>
2012-06-05 01:53:46 +0300

    Change database filesystem name.
    New name is more comfortable for filesystem usage with adb shell, for
    example.

--- application/src/main/java/app/android/gambit/local/DbOpenHelper.java
+++ application/src/main/java/app/android/gambit/local/DbOpenHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DbOpenHelper extends SQLiteOpenHelper
 {
 	private static final int DATABASE_VERSION = 1;
-	private static final String DATABASE_NAME = "Gambit";
+	private static final String DATABASE_NAME = "gambit.db";
 
 	public DbOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
