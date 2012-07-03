c963dc1e7e9cc4c57618cae592784cb5030a57e7
Artur Dryomov <me@ming13.ru>
2012-07-03 14:39:52 +0300

    Add basic GoogleDriveHelper class with authorization possibility.
    Solution for Android isn’t described in documentation, so current
    authorization solution is modified version of known one:
    http://stackoverflow.com/questions/11253768/how-to-load-file-on-google-drive

---
+++ application/src/main/java/app/android/gambit/remote/GoogleDriveHelper.java
@@ -0,0 +1,54 @@
+package app.android.gambit.remote;
+
+
+import java.io.IOException;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpRequest;
+import com.google.api.client.http.json.JsonHttpRequestInitializer;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveRequest;
+
+
+class GoogleDriveHelper
+{
+	private Drive driveService;
+
+	public GoogleDriveHelper(String authToken, String apiKey) {
+		driveService = buildDriveService(authToken, apiKey);
+	}
+
+	private Drive buildDriveService(String authToken, String apiKey) {
+		// TODO: Change to compatible transport
+		HttpTransport httpTransport = new NetHttpTransport();
+		JsonFactory jsonFactory = new JacksonFactory();
+		JsonHttpRequestInitializer requestInitializer = new DriveRequestInitializer(authToken, apiKey);
+
+		Drive.Builder driveServiceBuilder = new Drive.Builder(httpTransport, jsonFactory, null);
+		driveServiceBuilder.setJsonHttpRequestInitializer(requestInitializer);
+
+		return driveServiceBuilder.build();
+	}
+
+	private static class DriveRequestInitializer implements JsonHttpRequestInitializer
+	{
+		private final String authToken;
+		private final String apiKey;
+
+		public DriveRequestInitializer(String authToken, String apiKey) {
+			this.authToken = authToken;
+			this.apiKey = apiKey;
+		}
+
+		@Override
+		public void initialize(JsonHttpRequest jsonHttpRequest) throws IOException {
+			DriveRequest driveRequest = (DriveRequest) jsonHttpRequest;
+
+			driveRequest.setOauthToken(authToken);
+			driveRequest.setKey(apiKey);
+		}
+	}
+}
