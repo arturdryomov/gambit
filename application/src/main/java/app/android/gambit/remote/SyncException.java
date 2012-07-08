f43f6bc8e5fdcb16facf3fdffefd32ea10e93532
Artur Dryomov <me@ming13.ru>
2012-07-08 22:07:21 +0300

    Remove useless constructors.

--- application/src/main/java/app/android/gambit/remote/SyncException.java
+++ application/src/main/java/app/android/gambit/remote/SyncException.java
@@ -8,12 +8,4 @@ public class SyncException extends RuntimeException
 	public SyncException(String message) {
 		super(message);
 	}
-
-	public SyncException(Throwable cause) {
-		super(cause);
-	}
-
-	public SyncException(String message, Throwable cause) {
-		super(message, cause);
-	}
 }
