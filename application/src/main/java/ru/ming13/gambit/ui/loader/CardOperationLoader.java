4b742d42f18f7462eb43cbf978d2d5d8f1742c8c
Artur Dryomov <artur.dryomov@gmail.com>
2012-08-12 18:41:14 +0300

    Add operations loader: for decks and for cards.

---
+++ application/src/main/java/ru/ming13/gambit/ui/loader/CardOperationLoader.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2012 Artur Dryomov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.ming13.gambit.ui.loader;
+
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import ru.ming13.gambit.local.Card;
+import ru.ming13.gambit.local.Deck;
+import ru.ming13.gambit.ui.loader.result.LoaderResult;
+import ru.ming13.gambit.ui.loader.result.LoaderStatus;
+
+
+public class CardOperationLoader extends AsyncTaskLoader<LoaderResult<Card>>
+{
+	private static enum Operation
+	{
+		CREATE, MODIFY, DELETE
+	}
+
+	private final Operation operation;
+
+	private Deck deck;
+	private Card card;
+	private String frontSideText;
+	private String backSideText;
+
+	public static CardOperationLoader newCreationLoader(Context context, Deck deck, String frontSideText, String backSideText) {
+		CardOperationLoader cardOperationLoader = new CardOperationLoader(context, Operation.CREATE);
+
+		cardOperationLoader.deck = deck;
+		cardOperationLoader.frontSideText = frontSideText;
+		cardOperationLoader.backSideText = backSideText;
+
+		return cardOperationLoader;
+	}
+
+	private CardOperationLoader(Context context, Operation operation) {
+		super(context);
+
+		this.operation = operation;
+	}
+
+	public static CardOperationLoader newModificationLoader(Context context, Card card, String frontSideText, String backSideText) {
+		CardOperationLoader cardOperationLoader = new CardOperationLoader(context, Operation.MODIFY);
+
+		cardOperationLoader.card = card;
+		cardOperationLoader.frontSideText = frontSideText;
+		cardOperationLoader.backSideText = backSideText;
+
+		return cardOperationLoader;
+	}
+
+	public static CardOperationLoader newDeletionLoader(Context context, Deck deck, Card card) {
+		CardOperationLoader cardOperationLoader = new CardOperationLoader(context, Operation.DELETE);
+
+		cardOperationLoader.deck = deck;
+		cardOperationLoader.card = card;
+
+		return cardOperationLoader;
+	}
+
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+
+		forceLoad();
+	}
+
+	@Override
+	public LoaderResult<Card> loadInBackground() {
+		switch (operation) {
+			case CREATE:
+				return createCard();
+
+			case MODIFY:
+				return modifyCard();
+
+			case DELETE:
+				return deleteCard();
+
+			default:
+				throw new LoaderException();
+		}
+	}
+
+	private LoaderResult<Card> createCard() {
+		card = deck.createCard(frontSideText, backSideText);
+
+		return buildSuccessResult();
+	}
+
+	private LoaderResult<Card> buildSuccessResult() {
+		return new LoaderResult<Card>(LoaderStatus.SUCCESS, card, new String());
+	}
+
+	private LoaderResult<Card> modifyCard() {
+		card.setFrontSideText(frontSideText);
+		card.setBackSideText(frontSideText);
+
+		return buildSuccessResult();
+	}
+
+	private LoaderResult<Card> deleteCard() {
+		deck.deleteCard(card);
+
+		return buildSuccessResult();
+	}
+}
---
+++ application/src/main/java/ru/ming13/gambit/ui/loader/DeckOperationLoader.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2012 Artur Dryomov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.ming13.gambit.ui.loader;
+
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import ru.ming13.gambit.R;
+import ru.ming13.gambit.local.AlreadyExistsException;
+import ru.ming13.gambit.local.DbProvider;
+import ru.ming13.gambit.local.Deck;
+import ru.ming13.gambit.ui.loader.result.LoaderResult;
+import ru.ming13.gambit.ui.loader.result.LoaderStatus;
+
+
+public class DeckOperationLoader extends AsyncTaskLoader<LoaderResult<Deck>>
+{
+	private static enum Operation
+	{
+		CREATE, RENAME, DELETE
+	}
+
+	private final Operation operation;
+
+	private Deck deck;
+	private String deckTitle;
+
+	public static DeckOperationLoader newCreationLoader(Context context, String deckTitle) {
+		DeckOperationLoader deckOperationLoader = new DeckOperationLoader(context, Operation.CREATE);
+
+		deckOperationLoader.deckTitle = deckTitle;
+
+		return deckOperationLoader;
+	}
+
+	private DeckOperationLoader(Context context, Operation operation) {
+		super(context);
+
+		this.operation = operation;
+	}
+
+	public static DeckOperationLoader newRenamingLoader(Context context, Deck deck, String deckTitle) {
+		DeckOperationLoader deckOperationLoader = new DeckOperationLoader(context, Operation.RENAME);
+
+		deckOperationLoader.deck = deck;
+		deckOperationLoader.deckTitle = deckTitle;
+
+		return deckOperationLoader;
+	}
+
+	public static DeckOperationLoader newDeletionLoader(Context context, Deck deck) {
+		DeckOperationLoader deckOperationLoader = new DeckOperationLoader(context, Operation.DELETE);
+
+		deckOperationLoader.deck = deck;
+
+		return deckOperationLoader;
+	}
+
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+
+		forceLoad();
+	}
+
+	@Override
+	public LoaderResult<Deck> loadInBackground() {
+		switch (operation) {
+			case CREATE:
+				return createDeck();
+
+			case RENAME:
+				return renameDeck();
+
+			case DELETE:
+				return deleteDeck();
+
+			default:
+				throw new LoaderException();
+		}
+	}
+
+	private LoaderResult<Deck> createDeck() {
+		try {
+			deck = DbProvider.getInstance().getDecks().createDeck(deckTitle);
+
+			return buildSuccessResult();
+		}
+		catch (AlreadyExistsException e) {
+			String errorMessage = getContext().getString(R.string.error_deck_already_exists);
+
+			return buildErrorResult(errorMessage);
+		}
+	}
+
+	private LoaderResult<Deck> buildSuccessResult() {
+		return new LoaderResult<Deck>(LoaderStatus.SUCCESS, deck, new String());
+	}
+
+	private LoaderResult<Deck> buildErrorResult(String errorMessage) {
+		return new LoaderResult<Deck>(LoaderStatus.ERROR, deck, errorMessage);
+	}
+
+	private LoaderResult<Deck> renameDeck() {
+		try {
+			deck.setTitle(deckTitle);
+
+			return buildSuccessResult();
+		}
+		catch (AlreadyExistsException e) {
+			String errorMessage = getContext().getString(R.string.error_deck_already_exists);
+
+			return buildErrorResult(errorMessage);
+		}
+	}
+
+	private LoaderResult<Deck> deleteDeck() {
+		DbProvider.getInstance().getDecks().deleteDeck(deck);
+
+		return buildSuccessResult();
+	}
+}
