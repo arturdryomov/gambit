244e34a8ae6384c87f46cdaab87fe73ae1a8480c
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2012-09-29 18:52:36 +0300

    Update database scheme.

---
+++ application/src/main/java/ru/ming13/gambit/local/sqlite/DbVersions.java
@@ -0,0 +1,12 @@
+package ru.ming13.gambit.local.sqlite;
+
+
+public final class DbVersions
+{
+	private DbVersions() {
+	}
+
+	public static final int CURRENT = 3;
+	public static final int LATEST_WITH_UPDATE_TIME_SUPPORT = 2;
+	public static final int LATEST_WITH_CAMEL_NAMING_STYLE = 1;
+}
