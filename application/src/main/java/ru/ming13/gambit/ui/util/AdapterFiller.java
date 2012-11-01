c03fb1e3efb9314614c3d7f30b847067f6a4c2b2
Artur Dryomov <artur.dryomov@gmail.com>
2012-11-01 16:09:29 +0300

    Rename compatibility adapter filling methods.

--- application/src/main/java/ru/ming13/gambit/ui/util/AdapterFiller.java
+++ application/src/main/java/ru/ming13/gambit/ui/util/AdapterFiller.java
@@ -13,19 +13,19 @@ public final class AdapterFiller
 	}
 
 	public static <T> void fill(ArrayAdapter<T> adapter, Collection<T> collection) {
-		if (isSystemArrayAdapterBatchFillingAvailable()) {
+		if (isSystemArrayAdapterAddAllMethodAvailable()) {
 			adapter.addAll(collection);
 		}
 		else {
-			batchFillAdapter(adapter, collection);
+			addAll(adapter, collection);
 		}
 	}
 
-	private static boolean isSystemArrayAdapterBatchFillingAvailable() {
+	private static boolean isSystemArrayAdapterAddAllMethodAvailable() {
 		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
 	}
 
-	public static <T> void batchFillAdapter(ArrayAdapter<T> adapter, Collection<T> collection) {
+	public static <T> void addAll(ArrayAdapter<T> adapter, Collection<T> collection) {
 		adapter.setNotifyOnChange(false);
 
 		for (T item : collection) {
