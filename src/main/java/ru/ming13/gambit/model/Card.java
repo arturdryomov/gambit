469b0bcec1c2b2851ecc84ae262fecc9efb719de
Artur Dryomov <artur.dryomov@gmail.com>
2014-01-31 16:22:59 +0300

    Change models to implement parcelable interface.

--- src/main/java/ru/ming13/gambit/model/Card.java
+++ src/main/java/ru/ming13/gambit/model/Card.java
@@ -1,6 +1,9 @@
 package ru.ming13.gambit.model;
 
-public class Card
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Card implements Parcelable
 {
 	private final String frontSideText;
 	private final String backSideText;
@@ -17,4 +20,32 @@ public class Card
 	public String getBackSideText() {
 		return backSideText;
 	}
+
+	public Card(Parcel parcel) {
+		this.frontSideText = parcel.readString();
+		this.backSideText = parcel.readString();
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel parcel, int flags) {
+		parcel.writeString(frontSideText);
+		parcel.writeString(backSideText);
+	}
+
+	public static final Creator<Card> CREATOR = new Creator<Card>() {
+		@Override
+		public Card createFromParcel(Parcel parcel) {
+			return new Card(parcel);
+		}
+
+		@Override
+		public Card[] newArray(int size) {
+			return new Card[size];
+		}
+	};
 }
--- src/main/java/ru/ming13/gambit/model/Deck.java
+++ src/main/java/ru/ming13/gambit/model/Deck.java
@@ -1,6 +1,9 @@
 package ru.ming13.gambit.model;
 
-public class Deck
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Deck implements Parcelable
 {
 	private final String title;
 
@@ -11,4 +14,31 @@ public class Deck
 	public String getTitle() {
 		return title;
 	}
+
+	public Deck(Parcel parcel) {
+		this.title = parcel.readString();
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel parcel, int flags) {
+		parcel.writeString(title);
+	}
+
+	public static final Creator<Deck> CREATOR = new Creator<Deck>()
+	{
+		@Override
+		public Deck createFromParcel(Parcel parcel) {
+			return new Deck(parcel);
+		}
+
+		@Override
+		public Deck[] newArray(int size) {
+			return new Deck[size];
+		}
+	};
 }
